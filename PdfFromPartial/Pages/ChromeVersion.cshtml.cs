769170d105dd8df76b4f6497b62787c7be926790
Mike Brind <mikebrind@stepcomms.com>
2022-11-26 09:03:48 +0000

    @import fonts

--- PdfFromPartial/Pages/ChromeVersion.cshtml.cs
+++ PdfFromPartial/Pages/ChromeVersion.cshtml.cs
@@ -1,3 +1,5 @@
+using AngleSharp.Dom;
+using AngleSharp.Io;
 using ChromeHtmlToPdfLib;
 using ChromeHtmlToPdfLib.Settings;
 using Microsoft.AspNetCore.Mvc;
@@ -21,17 +23,26 @@ namespace PdfFromPartial.Pages
             this.productManager = productManager;
             this.renderer = renderer;
         }
-
+        public List<Product> Products { get; set; } = new();
+        public string BaseHref => $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
         public async Task<FileResult> OnGetAsync()
         {
-            
+            Products = await productManager.GetProducts();
+            var html = await renderer.RenderPartialToStringAsync("_ProductReport-v2", this);
             using var converter = new Converter();
             var stream = new MemoryStream();
             var pageSettings = new PageSettings(ChromeHtmlToPdfLib.Enums.PaperFormat.A4);
-            var url = $"{Request.Scheme}://{Request.Host}{Url.Page("/Pdfs/ReorderReport")}";
-            converter.ConvertToPdf(new ConvertUri(url), stream, pageSettings);
+            converter.ConvertToPdf(html, stream, pageSettings);
             stream.Position = 0;
             return File(stream, MediaTypeNames.Application.Pdf, "Reorder Report (Chrome).pdf");
         }
     }
 }
+
+//using var converter = new Converter();
+//var stream = new MemoryStream();
+//var pageSettings = new PageSettings(ChromeHtmlToPdfLib.Enums.PaperFormat.A4);
+//var url = $"{Request.Scheme}://{Request.Host}{Url.Page("/Pdfs/ReorderReport")}";
+//converter.ConvertToPdf(new ConvertUri(url), stream, pageSettings);
+//stream.Position = 0;
+//return File(stream, MediaTypeNames.Application.Pdf, "Reorder Report (Chrome).pdf");
